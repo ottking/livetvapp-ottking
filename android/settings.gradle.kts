31cb4e6bfd4556a2407fa6bb89095a9da66df04d
OTTKING LIVE <ottkingbd@gmail.com>
2026-05-13 18:20:28 +0600

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,10 +19,15 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    // নিচের এই লাইনটি যোগ করুন
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    
+    // ভার্সনটি 8.1.0 থেকে বাড়িয়ে 8.3.2 করা হলো (এটি বেশি স্ট্যাবল)
+    id("com.android.application") version "8.3.2" apply false
+    
+    // কোটলিন ভার্সনটিও কিছুটা আপডেট করা ভালো
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    
+    // ফায়ারবেস গুগল সার্ভিস লেটেস্ট ভার্সন
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
 include(":app")
